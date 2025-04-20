b80ebd4b52ae8399983b8fdcae15c85f2de26e94
Cyrille DE GASQUET <pro.cyrille.degasquet@gmail.com>
2025-04-20 19:54:28 +0200

    Fixing de la connexion

--- src/main/java/com/planteshop/controller/GlobalUserTypeAdvice.java
+++ src/main/java/com/planteshop/controller/GlobalUserTypeAdvice.java
@@ -1,20 +1,45 @@
 package com.planteshop.controller;
 
 import com.planteshop.model.entity.User;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 public class GlobalUserTypeAdvice {
+	/**
+	 * Expose l’énum directement à la vue.
+	 * - VISITOR si pas connecté
+	 * - USER / ADMIN sinon
+	 */
+	// @ModelAttribute("userRole")
+	// public String userRole(@AuthenticationPrincipal User user) {
+	// 	try {
+	// 		return user.getRole().name().toLowerCase();
+	// 	} catch (Exception exception) {
+	// 		System.err.println("DEBUG : GlobalUserTypeAdvice.serRole - Error : "+exception);
+	// 		return "visitor";
+	// 	}
+	// }
 
-    @ModelAttribute("finalCurrentUserType")
-    public String finalCurrentUserType(@AuthenticationPrincipal User user) {
-        if (user == null) return "visitor";
-        return switch (user.getRole()) {
-            case ADMIN -> "admin";
-            case USER -> "user";
-            default -> "visitor";
-        };
-    }
+	// @ModelAttribute("userRole")
+	// public String userRole(@AuthenticationPrincipal User user) {
+	// 		return user.getRole().name().toLowerCase();
+	// }
+
+	@ModelAttribute("userRole")
+	public String userRole(Authentication authentication) {
+
+			if (authentication == null                       // pas de session
+					|| !authentication.isAuthenticated()         // pas encore validée
+					|| authentication instanceof AnonymousAuthenticationToken) {
+					return "visitor";
+			}
+
+			// on sait qu’on a notre entité User comme Principal
+			User user = (User) authentication.getPrincipal();
+			return user.getRole().name().toLowerCase();
+	}
 }
