eedc6d03aea3ff16d50ae3435368ec88db8f61d1
Cyrille DEGASQUET <pro.cyrille.degasquet@gmail.com>
2025-04-23 18:24:50 +0200

    Correction erreur console avec l'affichage du role dans admin index users

--- src/main/java/com/planteshop/controller/admin/AdminUserController.java
+++ src/main/java/com/planteshop/controller/admin/AdminUserController.java
@@ -13,44 +13,56 @@ import java.util.Optional;
 @RequestMapping("/admin/users")
 public class AdminUserController {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    public AdminUserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+	public AdminUserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
-    @GetMapping
-    public String index(Model model) {
-			model.addAttribute("users", userRepository.findAll()
-			.stream()
-			.sorted((u1, u2) -> u1.getRole() == RoleType.ADMIN && u2.getRole() != RoleType.ADMIN ? -1 :
-													 u1.getRole() != RoleType.ADMIN && u2.getRole() == RoleType.ADMIN ? 1 : 0)
-			.toList());
-        return "admin/users/index";
-    }
+	@GetMapping
+	public String index(Model model) {
+		model.addAttribute("users", userRepository.findAll()
+				.stream()
+				.sorted((u1, u2) -> u1.getRole() == RoleType.ADMIN && u2.getRole() != RoleType.ADMIN ? -1
+						: u1.getRole() != RoleType.ADMIN && u2.getRole() == RoleType.ADMIN ? 1 : 0)
+				.toList());
+		return "admin/users/index";
+	}
 
-    @GetMapping("/{id}/edit")
-    public String edit(@PathVariable Long id, Model model) {
-        Optional<User> userOpt = userRepository.findById(id);
-        if (userOpt.isEmpty()) return "redirect:/admin/users";
-        model.addAttribute("user", userOpt.get());
-        return "admin/users/edit";
-    }
+	@GetMapping("/{id}/edit")
+	public String edit(@PathVariable Long id, Model model) {
+		Optional<User> userOpt = userRepository.findById(id);
+		if (userOpt.isEmpty())
+			return "redirect:/admin/users";
+		model.addAttribute("user", userOpt.get());
+		return "admin/users/edit";
+	}
 
-    @PatchMapping("/{id}")
-    public String update(@PathVariable Long id, @ModelAttribute User formUser) {
-        userRepository.findById(id).ifPresent(user -> {
-            user.setEmail(formUser.getEmail());
-            user.setName(formUser.getName());
-            user.setRole(formUser.getRole());
-            userRepository.save(user);
-        });
-        return "redirect:/admin/users";
-    }
+	@PatchMapping("/{id}")
+	public String update(@PathVariable Long id, @ModelAttribute User formUser) {
+		userRepository.findById(id).ifPresent(user -> {
+			if (formUser.getEmail() != null && !formUser.getEmail().isBlank()) {
+				user.setEmail(formUser.getEmail());
+			}
+			if (formUser.getName() != null && !formUser.getName().isBlank()) {
+				user.setName(formUser.getName());
+			}
+			if (formUser.getRole() != null) {
+				user.setRole(formUser.getRole());
+			}
+			System.err.println("DEBUG : AdminUserController, @PatchMapping(\"/{id}\"), user : name - "+user.getName()+", email - "+user.getEmail()+", role - "+user.getRole());
+			userRepository.save(user);
+			User userTmp = userRepository.findById(id).orElse(null);
+			if (userTmp != null) {
+				System.err.println("DEBUG : AdminUserController, @PatchMapping(\"/{id}\"), userTmp : name - " + userTmp.getName() + ", email - " + userTmp.getEmail() + ", role - " + userTmp.getRole());
+			}
+		});
+		return "redirect:/admin/users";
+	}
 
-    @PostMapping("/{id}/delete")
-    public String delete(@PathVariable Long id) {
-        userRepository.deleteById(id);
-        return "redirect:/admin/users";
-    }
+	@PostMapping("/{id}/delete")
+	public String delete(@PathVariable Long id) {
+		userRepository.deleteById(id);
+		return "redirect:/admin/users";
+	}
 }
